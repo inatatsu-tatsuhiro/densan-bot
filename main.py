ffcf7215a62b23641d33502cfb1a421c0dc65c58
inatatsu-tatsuhiro <tatsuhiro.0906@gmail.com>
2020-09-26 15:40:13 +0900

    start set up

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ async def on_ready():
     print('ready')
     print('-----')
     os.system(f'curl -XPOST -d "token={os.environ["SLACK_TOKEN"]}" -d "channel=#{os.environ["SLACK_CHANNEL"]}" -d "text=densan botが起動しました。" "https://slack.com/api/chat.postMessage"')
+    guild = bot.get_guild(int(os.environ["GUILD_ID"]))
+    
+    ch1 = guild.get_channel(753978604051890207)
+    bot.observe_channels.add(ch1)
+    ch2 = guild.get_channel(753978640806445107)
+    bot.observe_channels.add(ch2)
+    ch3 = guild.get_channel(755235384744607856)
+    bot.observe_channels.add(ch3)
 
 for extension in EXTENSIONS:
     bot.load_extension(extension)
